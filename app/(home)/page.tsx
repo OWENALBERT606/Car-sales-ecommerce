29faa943a75bd0b04ea172b2200dd57a7178423f
resilience <owenalbert606@gmail.com>
2025-05-15 11:06:39 +0300

    removeing

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -13,17 +13,13 @@ import UserReviews from '@/components/frontend/shop/user-reviews'
 import LocationSlider from '@/components/locationSlider';
 import { useBanners } from '@/hooks/useBanners';
 import { useCategories } from '@/hooks/useCategories'
-import { useFarms } from '@/hooks/useFarms';
-import { useLocations } from '@/hooks/useLocations';
 import { useProducts } from '@/hooks/useProducts';
 import { Category } from '@prisma/client'
 import React from 'react'
 
 export default function page() {
   const { categories, error, isLoading } = useCategories();
-  const { farms} = useFarms();
    const {products}=useProducts();
-  const { locations} = useLocations();
   const { banners} = useBanners();
 
 
@@ -31,7 +27,7 @@ export default function page() {
     <div>
       <div className="container mx-auto px-4 md:px-12 lg:px-24 py-6">
         <div className="flex flex-col gap-6 md:flex-row">
-          <CategorySidebar farms={farms} />
+          {/* <CategorySidebar farms={farms} /> */}
           <div className="flex-1">
             <HeroSlider />
           </div>
@@ -39,7 +35,7 @@ export default function page() {
         <div className="my-6">
           <FeaturedCategories categories={categories} />
         </div>
-        <LocationSlider locations={locations}/>
+        {/* <LocationSlider locations={locations}/> */}
         <div className="">
           <h2 className="mb-6 text-2xl font-bold">Special Offers</h2>
           <PromotionalBanners banners={banners}/>
